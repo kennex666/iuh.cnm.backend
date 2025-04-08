ab1cb88177e510824025bdcb99e1f5e32c7f7ffe
LeNguyenDuyKhang <duykhang0175@gmail.com>
2025-04-08 21:24:26 +0700

    add: route of friend request

--- src/index.js
+++ src/index.js
@@ -12,4 +12,7 @@ app.use("/api/conversations", conversationRoute);
 
 const messageRoute = require("./routes/messageRoute");
 app.use("/api/messages", messageRoute);
+
+const friendRequestRoute = require("./routes/friendRequestRoute");
+app.use("/api/friendRequests", friendRequestRoute);
 module.exports = app;
