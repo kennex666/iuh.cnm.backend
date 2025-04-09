4a1f99701974c6472c6500492fdeacb8d4586674
LeNguyenDuyKhang <duykhang0175@gmail.com>
2025-04-09 19:16:56 +0700

    update: model of friendrequest

--- src/models/friendrequest-model.js
+++ src/models/friendrequest-model.js
@@ -2,22 +2,29 @@ const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 const typeRequest = require('./type-request'); // Import typeRequest từ file typeRequest.js
 const { create } = require('./message-model');
+const { generateIdSnowflake } = require("../utils/id-generators");
+
 
 // Định nghĩa schema cho FriendRequest
 const friendRequestSchema = new Schema({
-    id: { type: String },
+    id: { type: String ,default: generateIdSnowflake, unique: true },
     senderId: { type: String, required: true },
     receiverId: { type: String, required: true },
     status: { type: String, enum: typeRequest, default: 'pending' }, // 'peding' | 'accepted' | 'decline'
     createAt: { type: Date, default: Date.now },
     updateAt: { type: Date, default: Date.now },
 });
-friendRequestSchema.pre('save', function (next) {
-    if (this.isNew || this.id === undefined) {
-        this.id = this._id.toString();
-    }
-    next();
-});
+// friendRequestSchema.pre('save', function (next) {
+//     if (this.isNew || this.id === undefined) {
+//         this.id = this._id.toString();
+//     }
+//     next();
+// });
+
+friendRequestSchema.statics.findById = function (id) {
+	return this.findOne({ id: id })
+};
+
 friendRequestSchema.pre('save', function (next) {
     this.updateAt = Date.now();
     next();
