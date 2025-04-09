4d5fdee765744246cf43393c2fa703f51670f720
LeNguyenDuyKhang <duykhang0175@gmail.com>
2025-04-09 19:18:09 +0700

    update: route of conversation

--- src/routes/conversation-route.js
+++ src/routes/conversation-route.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const {getAllConversationsController, getConversationByIdController, createConversationController, updateConversationController, deleteConversationController} = require("../controllers/conversation-controller");
-
+const authMiddleware = require("../middlewares/auth");
 const conversationRoute = express.Router();
 
 //get all conversations
-conversationRoute.get("/", getAllConversationsController);
+conversationRoute.get("/",authMiddleware, getAllConversationsController);
 //get conversation by id
-conversationRoute.get("/:id", getConversationByIdController);
+conversationRoute.get("/:id", authMiddleware, getConversationByIdController);
 //save conversation
-conversationRoute.post("/", createConversationController);
+conversationRoute.post("/", authMiddleware, createConversationController);
 //update conversation
-conversationRoute.put("/:id", updateConversationController);
+conversationRoute.put("/:id", authMiddleware, updateConversationController);
 //delete conversation
-conversationRoute.delete("/:id", deleteConversationController);
+conversationRoute.delete("/:id", authMiddleware, deleteConversationController);
 
 
 module.exports = conversationRoute;
