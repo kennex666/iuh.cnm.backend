468c597a75b4bc0ee7b82c9f8763063087e5d808
LeNguyenDuyKhang <duykhang0175@gmail.com>
2025-04-09 19:18:44 +0700

    update: route of friendrequest

--- src/routes/friendrequest-route.js
+++ src/routes/friendrequest-route.js
@@ -1,25 +1,25 @@
 const express = require("express");
 const { getAllFriendRequestsController, getFriendRequestByIdController, createFriendRequestController, updateFriendRequestAcceptController, updateFriendRequestDeclineController, deleteFriendRequestController, getAllPendingFriendRequestsController, getAllAcceptedFriendRequestsController, getAllDeclinedFriendRequestsController } = require("../controllers/friendRequest-controller");
-
+const authMiddleware = require("../middlewares/auth");
 const friendRequestRoute = express.Router();
 
 //get all friend requests
-friendRequestRoute.get("/", getAllFriendRequestsController);
+friendRequestRoute.get("/", authMiddleware, getAllFriendRequestsController);
 //get friend request by id
-friendRequestRoute.get("/:id", getFriendRequestByIdController);
+friendRequestRoute.get("/:id",  authMiddleware,getFriendRequestByIdController);
 //save friend request
-friendRequestRoute.post("/", createFriendRequestController);
+friendRequestRoute.post("/",  authMiddleware,createFriendRequestController);
 //update friend request
-friendRequestRoute.put("/accept/:id", updateFriendRequestAcceptController);
+friendRequestRoute.put("/accept/:id",  authMiddleware,updateFriendRequestAcceptController);
 //update friend request
-friendRequestRoute.put("/decline/:id", updateFriendRequestDeclineController);
+friendRequestRoute.put("/decline/:id",  authMiddleware,updateFriendRequestDeclineController);
 //delete friend request
-friendRequestRoute.delete("/:id", deleteFriendRequestController);
+friendRequestRoute.delete("/:id",  authMiddleware,deleteFriendRequestController);
 //get friend request status is pending
-friendRequestRoute.get("/pending/:id", getAllPendingFriendRequestsController);
+friendRequestRoute.get("/pending/:id",  authMiddleware,getAllPendingFriendRequestsController);
 //get friend request status is accept
-friendRequestRoute.get("/accepted/:id", getAllAcceptedFriendRequestsController);
+friendRequestRoute.get("/accepted/:id",  authMiddleware,getAllAcceptedFriendRequestsController);
 //get friend request status is decline
-friendRequestRoute.get("/decline/:id", getAllDeclinedFriendRequestsController);
+friendRequestRoute.get("/decline/:id",  authMiddleware,getAllDeclinedFriendRequestsController);
 
 module.exports = friendRequestRoute;
